bdbd331cf7c97f857eba1b65136f399dbe9ab211
yamamech <30635996+yamamech@users.noreply.github.com>
2023-08-19 13:02:12 +0100

    Removed zicross dependency, now compiles on MacOS with zig 0.11.0

--- flake.nix
+++ flake.nix
@@ -1,42 +1,26 @@
 {
   inputs = {
-    zicross.url = github:flyx/Zicross;
     nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
   };
-  outputs = {self, zicross, nixpkgs-unstable, flake-utils}:
-      with flake-utils.lib; eachSystem allSystems (system: let
-    pkgs = import nixpkgs-unstable {
-      inherit system;
-      overlays = [
-        zicross.overlays.zig
-      ];
-    };
-  in rec {
+  outputs = {self, nixpkgs-unstable, flake-utils}:
+		with flake-utils.lib; eachSystem allSystems (system: {
     packages = rec {
-      zig-nix-deez = pkgs.buildZig {
-        buildInputs = [ ];
-        pname = "zig-nix-deez";
-        version = "0.1.0";
-        src = ./.;
-        zigExecutables = [
-          {
-            name = "zig-nix-deez";
-            file = "main.zig";
-            install = true;
-          }
-        ];
-        zigTests = [
-        ];
-        meta = {
-          maintainers = [ "Yamamech <30635996+yamamech@users.noreply.github.com>" ];
-          description = "zig-nix-deez";
-        };
-      };
+      zig-nix-deez = with import nixpkgs-unstable { inherit system; }; stdenv.mkDerivation {
+				name = "zig-nix-deez";
+				src = ./.;
+				buildInputs = [ zig ];
+				buildPhase = ''
+					export ZIG_LOCAL_CACHE_DIR=$(pwd)/zig-cache
+					export ZIG_GLOBAL_CACHE_DIR=$ZIG_LOCAL_CACHE_DIR
+					zig build $ADDITIONAL_FLAGS
+				'';
+				installPhase = ''
+					mkdir -p $out/bin
+					cp zig-out/bin/zig-nix-deez $out/bin/
+				'';
+			};
       default = zig-nix-deez;
-    };
-    overlay = final: prev: {
-      zig-nix-deez = pkgs.callPackage self.packages.zig-nix-deez;
     };
   });
 }
